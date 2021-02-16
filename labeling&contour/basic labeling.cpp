a4c61c771a2cf6d82948d51d0862efe3dac3fcbb
kvmii <wjdaks915@khu.ac.kr>
2021-02-16 20:12:35 +0900

    Create basic labeling.cpp

---
+++ labeling&contour/basic labeling.cpp
@@ -0,0 +1,29 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+void labeling_basic() {
+	uchar data[] = {
+		0, 0, 1, 1, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 1, 0,
+		1, 1, 1, 1, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 1, 1, 0,
+		0, 0, 0, 1, 1, 1, 1, 0,
+		0, 0, 0, 1, 0, 0, 1, 0,
+		0, 0, 1, 1, 1, 1, 1, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+	};
+
+	Mat src = Mat(8, 8, CV_8UC1, data) * 255;
+
+	Mat labels;
+
+	int cnt = connectedComponents(src, labels);
+
+	cout << "src:\n" << src << endl;
+	cout << "labels:\n" << labels << endl;
+	cout << "number of labels: " << cnt << endl;
+}
+
